e126ab9a177564561d460671dd5386967ef3afc8
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-13 01:42:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array.join("")
   elsif array.size == 2
     return array.join(" and ") 
-  elsif array.size == 3
+  elsif array.size == 1
     return array.join(", ")
   end
 end
