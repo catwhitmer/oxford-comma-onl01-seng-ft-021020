d6d0592efcb5b4941dbf5cedbf4b86b01ff9f2e8
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-13 02:05:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
+  
   if array.size == 2
     return array.join(" and ") 
-  elsif array.size == 1
-    return array.join(", ")
+  
   elsif array.size >= 3
     return array.join(" ,")
   end
