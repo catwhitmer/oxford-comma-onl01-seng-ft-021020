d0e94f38a18d7a67c9fbcdc28715953491dad6e3
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-13 01:50:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,21 +1,11 @@
 def oxford_comma(array)
-  array2 = []
-
-  if array.length == 1
-    puts array.join()
-
-  else if array.length == 2
-    puts "#{array.first} and #{array.last}"
-    
-  else
-  second_2_last = array.length - 1
-  count = 0
-  while count != second_2_last
-      array2.push(array[count])
-      count += 1
+  if array.size == 0
+    return array.join("")
+  elsif array.size == 2
+    return array.join(" and ") 
+  elsif array.size == 1
+    return array.join(", ")
+  elsif array.size >= 3
+    return array.join()
   end
-  str_1 = array2.join(', ')
-  puts str_1 + ", and #{array.last}"
-end
-end
 end
